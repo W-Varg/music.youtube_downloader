3dcd18a4bf32c6e2ed9562a2ee34cce7cbf19e8c
Wilver Vargas <kryshot05@gmail.com>
2023-07-21 19:02:12 -0400

    update style and js of extension

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,41 +1,68 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const copyButton = document.getElementById('copyButton');
-  copyButton.addEventListener('click', performRequestAndShowAlert);
+  const youtubeMusicButton = document.getElementById('youtubeMusicButton');
+  const youtubeButton = document.getElementById('youtubeButton');
+
+  const directoryInput = document.getElementById('directoryInput');
+  // Recuperar el valor del directorio del almacenamiento local
+  chrome.storage.local.get('directory', function (result) {
+    if (result.directory) {
+      directoryInput.value = result.directory;
+    }
+  });
+
+  youtubeMusicButton.addEventListener('click', downloadFromYouTubeMusic);
+  youtubeButton.addEventListener('click', downloadFromYouTube);
+
 });
 
-function performRequestAndShowAlert() {
-  // Buscar la pestaña que contiene "youtube.com" y obtener su URL completa
-  chrome.tabs.query({ url: '*://*.youtube.com/*' }, function (tabs) {
-    if (tabs.length > 0) {
-      const youtubeURL = tabs[0].url;
-      const directory = "/home/dev/Music";
+function downloadFromYouTubeMusic() {
+  const directoryInput = document.getElementById('directoryInput');
+  const directory = directoryInput.value ?? '/home/dev/Music';
 
-      // Realizar la petición al servidor
-      fetch('http://127.0.0.1:5000/', {
-        method: 'POST', headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          "link": youtubeURL,
-          "directory": directory
-        })
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('La petición no fue exitosa.');
-          }
-          return response.json();
-        })
-        .then(data => {
-          showAlert(data.message || 'Petición completada.');
-        })
-        .catch(() => {
-          showAlert('Error al realizar la petición.');
-        });
-    } else {
-      showAlert('No se encontró ninguna pestaña de YouTube abierta.');
-    }
+  chrome.tabs.query({ url: '*://music.youtube.com/*' }, function (tabs) {
+    makeRequest(tabs, directory, 'YouTube Music');
   });
 }
 
+function downloadFromYouTube() {
+  const directoryInput = document.getElementById('directoryInput');
+  const directory = directoryInput.value ?? '/home/dev/Music/youtube';
+
+  chrome.tabs.query({ url: '*://youtube.com/*' }, function (tabs) {
+    makeRequest(tabs, directory, 'YouTube');
+  });
+}
+
+function makeRequest(tabs, directory, text) {
+  chrome.storage.local.set({ 'directory': directory });
+
+  if (tabs.length > 0) {
+    const youtubeURL = tabs[0].url;
+
+    fetch('http://127.0.0.1:5000/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        link: youtubeURL,
+        directory: directory,
+      }),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('La petición no fue exitosa.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        showAlert(data.message || 'Petición completada.');
+      })
+      .catch(() => {
+        showAlert('Error al realizar la petición.');
+      });
+  } else {
+    showAlert(`No se encontró ninguna pestaña de ${text} abierta.`);
+  }
+}
 
 function showAlert(message) {
   alert(message);
